160ed9ca744781c0d378bacc6b7f1a89fbbdc809
Corentin Séchet <gdaow@users.noreply.github.com>
2022-01-15 02:23:23 +0100

    feat(utils): Add hashmap from Joshua J Baker

--- src/test.c
+++ src/test.c
@@ -1,8 +1,32 @@
 #include "wxc/app.h"
 #include <stdio.h>
 
+struct TestViewModelData {
+    char* text_value;
+}
+
+void set_text_value(WlgViewModel* view_model, char* value) {
+    TestViewModelData* data = (TestViewModelData*)wlg_view_model_get_data(view_model);
+    data->text_value = value;
+}
+
+char* get_text_value(WlgViewModel* view_model) {
+    TestViewModelData* data = (TestViewModelData*)wlg_view_model_get_data(view_model);
+    return data->text_value;
+}
+
 bool on_init(WxcApp* app, void* data) {
-    const char* test = (const char*)data;
+    Wlg* wlg = wlg_init();
+    WlgTemplate* template = wlg_template_from_string(
+        wlg,
+        "frame:"
+        "  box:"
+        "    - button:"
+        "        label: 'Hello World'"
+    );
+
+    WlgFrame* frame = wlg_template_instanciate(template, NULL);
+    wlg_frame_show(frame, true);
     return wxc_app_on_init_parent(app);
 }
 
