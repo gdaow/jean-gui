fddd31047d80dfc53d3c2658c0946cd0864701b1
Corentin Séchet <gdaow@users.noreply.github.com>
2022-03-10 10:18:30 +0100

    doc(object-model): Document jg_member.h

--- modules/jgui/include/jgui/object-model/member.h
+++ modules/jgui/include/jgui/object-model/member.h
@@ -5,8 +5,8 @@
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
  * 
- * TODO:
- *  - document.
+ * Members encapsulate function pointers and can be registered into
+ * jg_class for dynamic method calling and property getting / setting.
  *
  */
 #ifndef JGUI_OBJECT_MODEL_MEMBER_H
@@ -17,12 +17,19 @@
 jg_forward_declare(jg_value)
 jg_forward_declare(jg_arguments)
 
+/**
+ * Pointer to a dynamic method that can be registered in a jg_class.
+ */
 typedef jg_value (*jg_method)(jg_arguments*);
 
-/** Callback for voids property getter.*/
+/**
+ * Pointer to a property getter that can be registered in a jg_class.
+ */
 typedef jg_value (*jg_getter)(const void*);
 
-/** Callback for voids property setter.*/
+/**
+ * Pointer to a property setter that can be registered in a jg_class.
+ */
 typedef void (*jg_setter)(void*, const jg_value);
 
 #endif
--- modules/jgui/private/object-model/member.h
+++ modules/jgui/private/object-model/member.h
@@ -5,8 +5,9 @@
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
  *
- * TODO:
- *  -document
+ * Private declarations for jgui/include/object-model/member.h.
+ * See public include for documentation.
+ *
  */
 #ifndef JGUI_PRIVATE_OBJECT_MODEL_MEMBER_H
 #define JGUI_PRIVATE_OBJECT_MODEL_MEMBER_H
@@ -16,16 +17,58 @@
 
 jg_forward_declare(jg_member)
 
+/**
+ * Initialize a member as a method wrapping the given function pointer.
+ *
+ * @param method Pointer to a function this member is bound to.
+ *
+ * @return The wrapped int value.
+ */
 void jg_method_init(jg_member* member, jg_method method);
 
+/**
+ * Call the given member with the given arguments.
+ *
+ * If the member isn't a method, the behavior is undefined.
+ *
+ * @param arguments Arguments to call the method with.
+ *
+ * @return The wrapped return value of the method call.
+ */
 jg_value jg_method_call(jg_member* member, jg_arguments* arguments);
 
+/**
+ * Initialize a member as a property using the given getter and setter.
+ *
+ * @param getter Pointer to the property getter.
+ * @param setter Pointer to the property setter.
+ */
 void jg_property_init(jg_member* member, jg_getter getter, jg_setter setter);
 
+/**
+ * Get the value of the given property on the given object.
+ *
+ * If the member isn't a property, the behavior is undefined.
+ *
+ * @param object Object to get the property on.
+ *
+ * @return The property value.
+ */
 jg_value jg_property_get(void* object, jg_member* member);
 
+/**
+ * Set the value of the given property on the given object.
+ *
+ * If the member isn't a property, the behavior is undefined.
+ *
+ * @param object Object to set the property on.
+ * @param value  The property value to set.
+ */
 void jg_property_set(void* object, jg_member* member, jg_value value);
 
+/**
+ * Releases memory used by a member.
+ */
 void jg_member_cleanup(void* member);
 
 typedef struct jg_member_s {
