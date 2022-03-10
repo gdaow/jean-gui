92fdb36f0078bad0b8079008a8cb78676b7f2184
Corentin Séchet <gdaow@users.noreply.github.com>
2022-03-10 23:21:13 +0100

    doc(object-model): Document jg_class methods

--- modules/jgui/include/jgui/object-model/class.h
+++ modules/jgui/include/jgui/object-model/class.h
@@ -5,8 +5,11 @@
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
  * 
- * TODO:
- *  - document.
+ * A class is a type definition, aggregating member declaration and informations
+ * nedded to instanciate objects. Class are not created directly, but rather
+ * registered in pluginsn during context context loading, by calling
+ * jg_context_add_class. Method and properties can be called / get /set using
+ * jg_object_call, jg_object_get and jg_object_set methods.
  *
  */
 #ifndef JGUI_PUBLIC_JGUI_CLASS_H
@@ -18,8 +21,27 @@
 jg_forward_declare(jg_class)
 jg_forward_declare(jg_arguments)
 
+/**
+  * Add a method to this class.
+  *
+  * If a property or a method with the same name is already registered in this
+  * class, the behavior is undefined.
+  *
+  * @param name   Name used to identify this method.
+  * @param method Pointer to the method function to bind to the method name.
+  */
 void jg_class_add_method(jg_class* class_, const char* name, jg_method method);
 
+/**
+  * Add a property to this class.
+  *
+  * If a property or a method with the same name is already registered in this
+  * class, the behavior is undefined.
+  *
+  * @param name   Name used to identify this property.
+  * @param getter Pointer to the getter function to bind to this property name.
+  * @param setter Pointer to the setter function to bind to this property name.
+  */
 void jg_class_add_property(jg_class* class_, const char* name, jg_getter getter, jg_setter setter);
 
 #endif
--- modules/jgui/private/object-model/class.h
+++ modules/jgui/private/object-model/class.h
@@ -5,8 +5,9 @@
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
  *
- * TODO:
- *  -document
+ * Private declarations for jgui/include/object-model/class.h.
+ * See public include for documentation.
+ *
  */
 #ifndef JGUI_PRIVATE_OBJECT_MODEL_CLASS_H
 #define JGUI_PRIVATE_OBJECT_MODEL_CLASS_H
@@ -21,12 +22,34 @@
 forward_declare(jg_class)
 forward_declare(jg_member)
 
+/**
+  * Initialize a class.
+  *
+  * @param parent Parent class, or NULL.
+  * @param size   Size of object this class describes. Usually sizeof(wrapped_c_type).
+  */
 void jg_class_init(jg_class* class_, const jg_class* parent, size_t size);
 
+/**
+  * Retrieve a member that was previously registered in this class using
+  * jg_class_add_method or jg_class_add_property. If the member is not defined on this
+  * class, it will be recursively searched in parent classes.
+  *
+  * @param member_name Name of the member to retrieve.
+  *
+  * @return A pointer to the retrieved member, or NULL if the member was not found in
+  *         this class or it's parents.
+  */
 const jg_member* jg_class_get_member(const jg_class* class_, const char* member_name);
 
+/**
+  * Release memory owned by this class instance.
+  */
 void jg_class_cleanup(void* class_);
 
+/**
+ * /!\ Private struct (see doc-private-struct) /!\
+ */
 struct jg_class_s {
     jg_index member_index;
     const jg_class* parent;
