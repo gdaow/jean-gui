3ecf262bad92e6e1cd4c1c91db6e60916b61a5c7
Corentin Séchet <gdaow@users.noreply.github.com>
2022-03-13 00:33:26 +0100

    doc(core): Document error_code.h (closes #16)

--- modules/jgui/include/jgui/core/error_code.h
+++ modules/jgui/include/jgui/core/error_code.h
@@ -5,21 +5,32 @@
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
  * 
- * TODO:
- *  - document.
+ * Context error code enum declaration.
  *
  */
 #ifndef JGUI_INCLUDE_JGUI_ERROR_CODE_H
 #define JGUI_INCLUDE_JGUI_ERROR_CODE_H
 
+/**
+ * Error code, usable in @ref jg_context_error and error hanlders set
+ * through @ref jg_context_set_error_handler.
+ */
 typedef enum {
+    /** No error occured. */
     JG_ERROR_NONE,
+
+    /** A requested class was not found. */
     JG_ERROR_CLASS_UNDEFINED,
 
+    /** Missing arguments during a dynamic method call. */
     JG_ERROR_MISSING_ARGUMENT,
+
+    /** Got a wrong argument type during a method call. */
     JG_ERROR_UNEXPECTED_ARGUMENT,
 
+    /** Last error code, for enumeration purposes. */
     JG_ERROR_LAST
 } jg_error_code;
 
 #endif
+
