6ed08b17ffba9137cd420d1585fb7423d480835c
Corentin Séchet <gdaow@users.noreply.github.com>
2022-03-13 01:02:20 +0100

    doc(core): Document id.h (closes #13)

--- modules/jgui/include/jgui/core/id.h
+++ modules/jgui/include/jgui/core/id.h
@@ -5,9 +5,9 @@
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
  * 
- * TODO:
- *  - document.
- *  - Use functions instead of macros here.
+ * jg_id is a generic identifier, consisting of a namespace and a name.
+ * It's used to avoid name collisions for objects named with a string in
+ * Jean-Gui (like classes).
  *
  */
 #ifndef JGUI_CORE_ID
@@ -15,16 +15,45 @@
 
 #include <stdbool.h>
 
+/**
+  * /!\ Private struct (see doc-private-struct) /!\
+  */
 typedef struct jg_id_s {
     const char* namespace_;
     const char* name;
 } jg_id;
 
+/** NULL identifier constant */
 extern const jg_id JG_ID_NONE;
 
+/*
+ * Create a new identifier.
+ *
+ * @param namespace_ The identifier namespace.
+ * @param name       The identifier name.
+ *
+ * @return A new id for the given namespace and name.
+ */
 jg_id JG_ID(const char* namespace_, const char* name);
 
+/*
+ * Check if an identifier is the NULL identifier.
+ *
+ * @param id The identifier to check.
+ *
+ * @return True if the given identifier is the NULL identifier, true otherwise.
+ */
 bool jg_id_is_none(jg_id id);
+
+/*
+ * Check if an identifier is correctly constructed.
+ * That is, either it's the NULL identifier, either both namespace and name are
+ * defined and not empty.
+ *
+ * @param id The identifier to check.
+ *
+ * @return True if the given identifier correctly constructed, false otherwise.
+ */
 bool jg_id_is_valid(jg_id id);
 
 #endif
